17649a62ac259799b5bc11f0802e66a9d05a90ad
rizkiatala25 <2303010058@unper.ac.id>
2026-06-15 18:17:41 +0700

    membuat halaman awal

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <template>
-  <div class="hello-container">
-    <h1>Hello World!</h1>
-    <p>{{ pesan }}</p>
-    <small>kuis</small>
-  </div>
+  <HomeView />
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-// Variabel reaktif untuk menampilkan pesan ke HTML di atas
-const pesan = ref('Mantap')
+import HomeView from './views/HomeView.vue'
 </script>
 
-<style scoped>
-/* Styling sederhana agar tulisan pas di tengah layar */
-.hello-container {
-  font-family: sans-serif;
-  text-align: center;
-  margin-top: 150px;
-  padding: 20px;
-}
-
-h1 {
-  color: #42b883; /* Warna hijau khas Vue */
-  font-size: 3rem;
-  margin-bottom: 10px;
-}
-
-p {
-  color: #2c3e50;
-  font-size: 1.2rem;
-}
-
-small {
-  color: #95a5a6;
-  display: block;
-  margin-top: 40px;
+<style>
+/* Reset margin default browser agar web full screen */
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #1a1c29;
 }
 </style>
